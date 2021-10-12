895c637d5d3ee00c4b31ca8a473adbfbdb1b5499
ArcOmotola <ogunsola40@gmail.com>
2021-10-12 20:53:23 +0100

    added nav, slider, shopping category on homepage

---
+++ src/components/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Products = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Products;
+
+const Container = styled.div``;
