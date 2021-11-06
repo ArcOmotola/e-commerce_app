3b5f1aaf46719716113d0eb893b6f9edfa38348d
ArcOmotola <ogunsola40@gmail.com>
2021-11-06 19:41:09 +0100

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,35 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 const App = () => {
+  const user = true;
+
   return (
     <div>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />
+          </Route>
+          <Route path="/products/:id">
+            <Product />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/login">
+            { user ? <Redirect to="/"/> : <Login />}
+          </Route>
+          <Route path="/register">
+            { user ? <Redirect to="/"/> : <Register />}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
